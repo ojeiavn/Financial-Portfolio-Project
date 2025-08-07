0cc9a33ac388a0ed9ab3d9a98bce9e41fb116100
Sreya Ramachandran <sreya66nambiar@gmail.com>
2025-08-07 15:48:02 +0100

    Add live clock to display current date and time

--- frontend/src/components/UserPortfolio.jsx
+++ frontend/src/components/UserPortfolio.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import UserStocks from "./UserStocks";
 import UserOtherAssests from "./UserOtherAssests";
 import UserCharts from "./UserCharts";
+import LiveClock from "./LiveClock";
 
 const UserPortfolio = () => {
     return (
         <div className="flex flex-col h-screen items-center bg-bg-light p-5 overflow-hidden font-main">
             {/* Heading */}
-            <div className="bg-bg-dark px-10 rounded-2xl py-6 text-white text-center">MY PORTFOLIO</div>
+            <div className="bg-bg-dark px-10 rounded-2xl py-6 text-white text-center w-2/4">MY PORTFOLIO</div>
+            <div className="text-white w-screen text-end pr-20"><LiveClock /> </div>
+
             
             <div className="flex justify-between bg-bg-light m-0 p-6 text-white w-full overflow-y-auto">
                 
