3e243b1c4735b53dd22a4fd70cf6844d06387d5f
Ojei Imiavan <ojei1211@gmail.com>
2025-08-08 13:48:02 +0100

    Added buy buttons to each broduct card.

--- frontend/src/ProductCard.jsx
+++ frontend/src/ProductCard.jsx
@@ -5,6 +5,7 @@ export function ProductCard({ value, percentage, change, name, color, onClick })
       <div>{value}</div>
       <div style={{ color }}>({percentage}) {change}</div>
       <div className="company-name">{name}</div>
+      <button class="buy-buttons" type="button" onClick="">Buy</button>
     </div>
   );
 }
