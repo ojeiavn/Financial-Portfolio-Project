bb00f8cdc933323517bef4d9f1c33306324c4d50
Sreya <sreya.sr606@gmail.com>
2025-08-08 13:56:02 +0000

    Created user charts for bonds and cash

---
+++ frontend/src/components/UserChartsBonds.jsx
@@ -0,0 +1,65 @@
+import React, {useEffect, useState} from "react";
+import ApexCharts from 'apexcharts'
+import ReactApexChart from 'react-apexcharts'
+
+const UserChartsBonds = () => { 
+     
+    const [state, setState] = React.useState({
+          
+        series: [], // 44, 55, 13, 43, 22
+        options: {
+            chart: {
+                width: 380,
+                type: 'pie',
+            },
+            labels: [], // 'Team A', 'Team B', 'Team C', 'Team D', 'Team E'
+            legend: {
+                position: 'bottom',
+                labels: {
+                    colors: '#ffffff', 
+                }
+            },
+            responsive: [{
+                breakpoint: 480,
+                options: {
+                    chart: {
+                    width: 200
+                    },
+                    legend: {
+                        position: 'bottom', 
+                    },
+                }
+            }]
+        },  
+    });
+    
+    useEffect(() => {
+        fetch("http://172.30.0.198:5000/holdings" || "http://localhost:5000/holdings")
+        .then((res) =>  res.json())
+        .then((jsondata)=> {
+            const bondsOnly = jsondata.filter((item) => item.type === "Bond");
+            setState((prev) => ({
+                ...prev,
+                series: bondsOnly.map((i) => Number(i.Quantity)),
+                options: {
+                    ...prev.options,
+                    labels: bondsOnly.map((i) => i.Symbol),
+                },
+            }))
+        })
+        .catch((err) => {
+            setMsg("Failed fetch data:  "+err)
+        });
+    }, []);   
+ 
+
+    return (
+        <>
+            <div id="chart">
+                <ReactApexChart options={state.options} series={state.series} type="pie" width={300} />
+            </div>
+        </>
+    );
+}
+
+export default UserChartsBonds;
